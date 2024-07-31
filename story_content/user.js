2d17a6fff96be6d30e05b8d129edcea5be2fa37c
FABISANG641 <fabisang641@gmail.com>
2024-07-31 15:14:08 -0500

    Primeros_153_B1_31-07-24

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+var date = 'Assessment completed on ' + new Date().toLocaleTimeString('en-us', options);
+var player = GetPlayer();
+player.SetVar("SystemDate",date);
+}
+
+};
